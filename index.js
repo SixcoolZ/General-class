9478f3d84cbdbb77f5d14801dccdcc0e2036680b
sixcoolZ <sixtuskenneth@gmail.com>
2023-05-15 15:48:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function addingOfTwoNumbers(a,b){
+    let sum = 0;
+    sum = a + b;
+    return sum;
+}
+
+let result = addingOfTwoNumbers(5,6);
+console.log(result);
+
+
+let a = 5;
+let b = 10;
+let sum = a * b;
+
+console.log(sum);
